0e10f1babbcb37589a76ba6ac78dca2040b81bad
Gabriel <soaresgabriel552@gmail.com>
2022-11-12 11:43:16 -0300

    calculadora prompt

---
+++ README.md
@@ -0,0 +1,9 @@
+# Calculadora prompt
+
+Primeiro projeto que faço com Javascript, usei o método prompt e a função alert para fazer uma calculadora simples
+que calcula a média no final. Tem também um arquivo em html, mas é apenas para abrir 
+a página do código Javascript.
+
+Ferramenta usada:
+
+- Javascript
---
+++ calc.js
@@ -0,0 +1,11 @@
+let nota1= prompt("Digite o primeiro numero: ");
+let nota2= prompt("Digite o segundo numero: ");
+let nota3= prompt("Digite o terceiro numero: ");
+
+let soma = parseInt(nota1) + parseInt(nota2) + parseInt(nota3);
+alert("o resultado e: " + soma);
+
+let div= prompt("Deseja dividir por quanto? ");
+
+let media= soma / parseInt(div);
+alert(media);
